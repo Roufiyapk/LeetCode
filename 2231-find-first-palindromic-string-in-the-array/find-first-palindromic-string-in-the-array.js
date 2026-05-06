48660c6867ee463085db3f55324663edb070f599
Roufiyapk <pkroufiya9119@gmail.com>
2026-05-06 14:52:53 +0530

    Time: 28 ms (13.68%) | Memory: 63.4 MB (10.64%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+
+    for(let word of words){
+        let reversed=word.split('').reverse().join("")
+        if(word===reversed){
+            return word
+        }
+    }
+    return ""
+    
+};
