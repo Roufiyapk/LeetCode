8df08fd93f4b69bde23ff1157cc435c7ceee32e1
Roufiyapk <pkroufiya9119@gmail.com>
2026-07-29 09:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (85.07%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int AlternatingSum(int[] nums) {
+
+        int sum=0;
+        for(int i=0;i<nums.Length;i++){
+            if(i%2==0){
+                sum+=nums[i];
+
+            }else{
+                sum-=nums[i];
+            }
+        }
+        return sum;
+        
+    }
+}
